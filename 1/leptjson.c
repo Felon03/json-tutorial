d9531461a71e76fbb843dca49eb39af235e22817
FreedomLy <freedom.jfl@gmail.com>
2017-08-17 21:32:41 +0800

    add tutorial 1

---
+++ 1/leptjson.c
@@ -0,0 +1,95 @@
+﻿#include <stdlib.h>    /* NULL */
+#include <assert.h>    /* assert() */
+#include "leptjson.h"
+
+#define EXPECT(c, ch) do {assert(*c->json == (ch)); c->json++;} while(0)
+
+
+/**
+* 为了减少解析函数之间传递多个参数，把数据都放进一个lept_context结构体
+*/
+typedef struct {
+    const char* json;
+}lept_context;
+
+/**
+* 解析函数
+*/
+/* ws = *(%x20/%x09/%x0A/%x0D) */
+static void lept_parse_whitespace(lept_context* c)
+{
+    const char* p = c->json;
+    while(*p == ' '|| *p == '\t' || *p == '\n' || *p == '\r')
+        p++;
+    c->json = p;
+}
+
+/* null = "null" */
+static int lept_parse_null(lept_context* c, lept_value* v)
+{
+    EXPECT(c, 'n');
+    if(c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = LEPT_NULL;
+    return LEPT_PARSE_OK;
+}
+
+/* true = "true" */
+static int lept_parse_true(lept_context* c, lept_value* v)
+{
+    EXPECT(c, 't');
+    if(c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = LEPT_TRUE;
+    return LEPT_PARSE_OK;
+}
+
+/* false = "false" */
+static int lept_parse_false(lept_context* c, lept_value* v)
+{
+    EXPECT(c, 'f');
+    if(c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+        c->json += 4;
+        v->type = LEPT_FALSE;
+        return LEPT_PARSE_OK;
+}
+
+/* value = null/false/true */
+static int lept_parse_value(lept_context* c, lept_value* v)
+{
+    switch(*c->json)
+    {
+        case 'n': return lept_parse_null(c,v);
+        case '\0': return LEPT_PARSE_EXPECT_VALUE;
+        case 't': return lept_parse_true(c,v);
+        case 'f': return lept_parse_false(c, v);
+        default: return LEPT_PARSE_INVALID_VALUE;
+    }
+}
+
+int lept_parse(lept_value* v, const char* json)
+{
+    lept_context c;
+    int ret;
+    assert(v != NULL);
+    c.json = json;
+    v->type = LEPT_NULL;
+    lept_parse_whitespace(&c);
+    if((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK)
+    {
+        lept_parse_whitespace(&c);
+        // 若 json 在一个值之后，空白之后还有其它字符，则要返回 LEPT_PARSE_ROOT_NOT_SINGULAR
+        if(*c.json != '\0')    
+            ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
+    return ret;
+}
+
+lept_type lept_get_type(const lept_value* v)
+{
+    assert(v != NULL);
+    return v->type;
+}
---
+++ 1/leptjson.h
@@ -0,0 +1,50 @@
+﻿#ifndef LEPTJSON_H_
+#define LEPTJSON_H_
+
+/**
+* JSON中的6中数据类型，把true和false当作两种类型就是7种
+* 声明一个枚举类型，包含了JSON的7中数据类型
+*/
+typedef enum {
+    LEPT_NULL,
+    LEPT_FALSE,
+    LEPT_TRUE,
+    LEPT_NUMBER,
+    LEPT_STRING,
+    LEPT_ARRAY,
+    LEPT_OBJECT
+} lept_type;
+
+/**
+    * 声明JSON的数据结构。
+    * 实现一个树的数据结构，每个节点用lept_value结构体表示，将其称为一个JSON值
+    */
+typedef struct
+{
+    lept_type type;
+} lept_value;
+
+/**
+    * lept_parse返回的值为一下枚举值
+    * 无错误会返回LEPT_PARSE_OK
+    */
+    enum
+    {
+        LEPT_PARSE_OK = 0,
+        LEPT_PARSE_EXPECT_VALUE,
+        LEPT_PARSE_INVALID_VALUE,
+        LEPT_PARSE_ROOT_NOT_SINGULAR
+    };
+
+/**
+    * 解析JSON的API函数
+    * 传入的JSON文本是一个C字符串(空结尾字符串)
+    */
+int lept_parse(lept_value *v, const char *json);
+
+/**
+    * 访问结果函数，获取其类型
+    */
+lept_type lept_get_type(const lept_value *v);
+
+#endif
