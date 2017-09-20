5ccb8e431f1af2aebf9e848ecb53342b262105bf
FreedomLy <freedom.jfl@gmail.com>
2017-09-20 19:26:24 +0800

    add tutorial 5

--- 5/leptjson.h
+++ 5/leptjson.h
@@ -1,7 +1,7 @@
 #ifndef LEPTJSON_H_
 #define LEPTJSON_H_
 
-#include <stddef.h>    // size_t
+#include <stddef.h>    //size_t
 
 typedef enum {
     LEPT_NULL,
@@ -11,15 +11,18 @@ typedef enum {
     LEPT_STRING,
     LEPT_ARRAY,
     LEPT_OBJECT
-}lept_type;
+} lept_type;
 
-typedef struct {
+typedef struct lept_value lept_value;
+
+struct lept_value {
     union {
-        struct { char* s; size_t len; }s;    // string: null-terminated string, string length
-        double n;                            // number
+        struct {lept_value* e; size_t size;}a;    // array: elements, element count
+        struct {char* s; size_t len;}s;           // string: null-terminated string, string length
+        double n;                                 // number
     }u;
     lept_type type;
-}lept_value;
+};
 
 enum {
     LEPT_PARSE_OK = 0,
@@ -31,10 +34,11 @@ enum {
     LEPT_PARSE_INVALID_STRING_ESCAPE,
     LEPT_PARSE_INVALID_STRING_CHAR,
     LEPT_PARSE_INVALID_UNICODE_HEX,
-    LEPT_PARSE_INVALID_UNICODE_SURROGATE
+    LEPT_PARSE_INVALID_UNICODE_SURROGATE,
+    LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET
 };
 
-#define lept_init(v) do { (v)->type = LEPT_NULL;} while(0)
+#define lept_init(v) do { (v)->type = LEPT_NULL; } while(0)
 
 int lept_parse(lept_value* v, const char* json);
 
@@ -54,4 +58,7 @@ const char* lept_get_string(const lept_value* v);
 size_t lept_get_string_length(const lept_value* v);
 void lept_set_string(lept_value* v, const char* s, size_t len);
 
-#endif // LEPTJSON_H_
+size_t lept_get_array_size(const lept_value* v);
+lept_value* lept_get_array_element(const lept_value* v, size_t index);
+
+#endif    // LEPTJSON_H_
